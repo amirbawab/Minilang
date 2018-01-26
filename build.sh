5a6795efe1c7d24baa7f58253b63cc9fae91cee9
Amir Bawab <amirbawab@gmail.com>
2018-01-26 08:36:24 -0500

    Customized build script

--- build.sh
+++ build.sh
@@ -2,7 +2,19 @@
 
 BUILD_DIR="build/"
 
+echo ">> Cleaning old build (if any)"
 rm -rf $BUILD_DIR
+
+echo ">> Creating build directory: $PWD/$BUILD_DIR"
 mkdir -p $BUILD_DIR
+
+echo ">> Entering build directory: $PWD/$BUILD_DIR"
 cd $BUILD_DIR
-cmake .. && make
+
+echo ">> Running cmake ..."
+cmake ..
+
+echo ">> Running make ..."
+make
+
+echo ">> DONE"
