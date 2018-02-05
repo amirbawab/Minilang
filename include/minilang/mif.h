28dc464af1f4d2e5ec5f2e77dd90142d49e16848
Amir Bawab <amirbawab@gmail.com>
2018-02-05 15:21:48 -0500

    Added if statement

--- include/minilang/mif.h
+++ include/minilang/mif.h
@@ -1,18 +1,16 @@
 #pragma once
 
+#include <minilang/mstatement.h>
 #include <minilang/mscope.h>
 #include <minilang/mexpression.h>
-#include <minilang/mstatement.h>
-#include <vector>
 
 namespace mini {
-    class MWhile : public mini::MScope, public mini::MStatement {
+    class MIf : public mini::MScope, public mini::MStatement {
     private:
-
-        // While loop condition
+        // If condition
         mini::MExpression* m_condition = nullptr;
     public:
-        MWhile(mini::MExpression* condition) : m_condition(condition){}
+        MIf(mini::MExpression* condition) : m_condition(condition){}
 
         /**
          * Get if statement condition
@@ -21,4 +19,3 @@ namespace mini {
         mini::MExpression* getCondition() const { return m_condition;}
     };
 }
-
